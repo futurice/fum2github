4e052bc496dc37308155fba7d95115a58498dc2a
Oleg Grenrus <oleg.grenrus@iki.fi>
2015-07-01 22:15:50 +0300

    Improvements in swarm implementation

--- swarm/src/Data/Swarm.hs
+++ swarm/src/Data/Swarm.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE Safe #-}
 ----------------------------------------------------------------------------
 -- |
 -- Module      :  Data.Swarm
@@ -7,49 +8,93 @@
 -- Maintainer  :  Oleg Grenrus <oleg.grenrus@iki.fi>
 --
 -- Parallel IO working
+--
+-- TODO: Handle exceptions inside jobs.
 ----------------------------------------------------------------------------
-module Data.Swarm (seqSwarm, swarm, swarm') where
+module Data.Swarm
+  (
+  -- * Order presevering.
+    swarm, swarm'
+  -- * Unordered
+  , unorderedSwarm, unorderedSwarm'
+  -- * Sequential
+  , seqSwarm
+  ) where
 
+import Control.Exception
 import Control.Concurrent
 import Control.Concurrent.STM
-import Control.Concurrent.STM.TMChan
+import Control.Monad ((>=>))
+import Control.Concurrent.STM.TBMChan
 import Control.DeepSeq
 import Data.Foldable
 import Data.List
 import Data.Ord
 import Data.Traversable
 
--- | ALis for 'traverse'. Sequential version of 'swarm'.
+return' :: NFData a => a -> IO a
+return' = evaluate . force
+
+-- | ALias for 'traverse'. Sequential version of 'swarm'.
 seqSwarm :: NFData b => (a -> IO b) -> [a] -> IO [b]
-seqSwarm f = traverse (fmap force . f)
+seqSwarm f = traverse (f >=> return')
 
-type Index = Int
+type InputBus a = TBMChan (IO a)
+type OutputBus a = TVar [a]
+data Bus a = Bus !(InputBus a) !(OutputBus a)
 
-commitWork :: NFData a => TVar [(Index, a)] -> Index -> a -> IO ()
-commitWork results idx x = x `deepseq` atomically (modifyTVar results ((idx, x) : ))
+data SP b = SP {-UNPACK-} !Int !b
 
-worker :: NFData a => TMChan (Index, IO a) -> TVar [(Index, a)] -> IO ()
-worker chan results = do
-  x <- atomically $ readTMChan chan
+instance NFData b => NFData (SP b) where
+  rnf (SP _ x) = rnf x
+
+fst' :: SP b -> Int
+fst' (SP i _) = i
+
+snd' :: SP b -> b
+snd' (SP _ x) = x
+
+commitWork :: NFData a => TVar [a] -> a -> IO ()
+commitWork results x = x `deepseq` atomically act
+  where act = modifyTVar results (x : )
+
+worker :: NFData a => Bus a -> IO ()
+worker bus@(Bus chan results) = do
+  x <- atomically $ readTBMChan chan
   case x of
-    Nothing            -> return ()
-    Just (idx, action) -> action >>= commitWork results idx >> worker chan results
+    Nothing     -> return ()
+    Just action -> action >>= commitWork results >> worker bus
 
 -- | Like 'traverse', but IO work is spawned to multiple workers.
 swarm :: NFData b => (a -> IO b) -> [a] -> IO [b]
 swarm f as = do n <- getNumCapabilities; swarm' n f as
 
+-- | 'swarm' with swarm (worker pool) size.
 swarm' :: NFData b => Int -> (a -> IO b) -> [a] -> IO [b]
 swarm' n f as = do
+  let as' = zip [0 :: Int ..] as
+  let f' (idx, a) = SP idx `fmap` f a
+  bs <- unorderedSwarm' n f' as'
+  return $ map snd' $ sortBy (comparing fst') bs                          
+
+-- | Like 'swarm', but the order of elements is not preserved.
+unorderedSwarm :: NFData b => (a -> IO b) -> [a] -> IO [b]
+unorderedSwarm f as = do n <- getNumCapabilities; swarm' n f as
+
+-- | 'unorderedSwarm' with swarm size.
+unorderedSwarm' :: NFData b => Int -> (a -> IO b) -> [a] -> IO [b]
+unorderedSwarm' n f as = do
   let l = length as
-  chan <- newTMChanIO
+  chan <- newTBMChanIO (n * 10)
   ress <- newTVarIO []
   -- Spawn swarm
-  forM_ [1..n] $ \_ -> forkIO $ worker chan ress >> return ()
+  let bus = Bus chan ress
+  forM_ [1..n] $ \_ -> forkIO $ worker bus >> return ()
   -- Submit work
-  atomically $ traverse_ (\(idx, a) -> writeTMChan chan (idx, (f a))) (zip [0..] as) >> closeTMChan chan
+  traverse_ (\a -> atomically . writeTBMChan chan . f $ a) as
+  atomically $ closeTBMChan chan
   -- Wait for it to be completed
   atomically $ do
     bs <- readTVar ress
     check (length bs == l)
-    return $ map snd $ sortBy (comparing fst) bs
+    return bs
