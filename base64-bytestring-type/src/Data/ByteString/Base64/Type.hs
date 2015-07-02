24a496c8b9658f43b493835b349da68eee388622
Oleg Grenrus <oleg.grenrus@iki.fi>
2015-07-02 10:03:59 +0300

    Add instances to ByteString64

--- base64-bytestring-type/src/Data/ByteString/Base64/Type.hs
+++ base64-bytestring-type/src/Data/ByteString/Base64/Type.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE Trustworthy #-}
 ----------------------------------------------------------------------------
 -- |
 -- Module      :  Data.ByteString.Base64.Type
@@ -14,15 +16,22 @@ import Control.Applicative
 import Data.Aeson
 import Data.ByteString
 import Data.ByteString.Base64 as Base64
+import Data.Data
+import Data.Serialize
 import Data.Text.Encoding (encodeUtf8, decodeUtf8With)
 import Data.Text.Encoding.Error
 
 -- | Aeson serialisable bytestring. Uses base64 encoding.
 newtype ByteString64 = ByteString64 { getByteString64 :: ByteString }
-  deriving (Eq, Show, Ord)
+  deriving (Eq, Show, Ord, Data, Typeable)
 
 instance ToJSON ByteString64 where
   toJSON = toJSON . decodeUtf8With ignore  . Base64.encode . getByteString64
 
 instance FromJSON ByteString64 where
   parseJSON v = ByteString64 <$> withText "ByteString" (pure . decodeLenient .  encodeUtf8) v
+
+-- | 'ByteString64' is serialised as 'ByteString'
+instance Serialize ByteString64 where
+  put = put . getByteString64
+  get = ByteString64 <$> get
